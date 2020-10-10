e87278c9cdeb684aa9be22d2fbacf0ab2ed32536
wangyantao1 <wangyantao1@jd.com>
2020-10-11 02:16:06 +0800

    添加dubbo

--- mall-ums/mall-ums-admin/src/main/java/org/tech/ums/UmsApplication.java
+++ mall-ums/mall-ums-admin/src/main/java/org/tech/ums/UmsApplication.java
@@ -3,11 +3,13 @@ package org.tech.ums;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan({"org.tech.ums.mapper"})
 @EnableTransactionManagement
+@ImportResource({"classpath:spring-config.xml"})
 public class UmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(UmsApplication.class, args);
